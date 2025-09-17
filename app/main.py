681eaf2b4af5e5a71aecd95fcdd4c8b5fe401f3b
camille-eloah <camelogithub@gmail.com>
2025-09-17 14:48:37 -0300

    feat: structuring initial FastAPI routes

--- app/main.py
+++ app/main.py
@@ -9,21 +9,105 @@ app = FastAPI(
     version="0.1.0",
 )
 
-
+# ----------------------------
+# 1. Autenticação
+# ----------------------------
 
 @app.post("/user", response_class=HTMLResponse)
 def cadastrar_user():
     return
+    
 
-@app.get("/user", response_class=HTMLResponse)
-def obter_usuario():
+@app.post("/login", response_class=HTMLResponse)
+def login():
+    return
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('login'))
+
+
+@app.get("/obter_usuarios", response_class=HTMLResponse)
+def obter_usuarios():
+    return
+
+# ----------------------------
+# 2. Gerenciamento de Conta
+# ----------------------------
+@app.post("/editar_perfil", response_class=HTMLResponse)
+def editar_perfil():
     return 
+
+@app.delete("/excluir_conta", response_class=HTMLResponse)
+def excluir_conta():
+    return 
+# ----------------------------
+# 3. Aulas
+# ----------------------------
+
+@app.post("/", response_class=HTMLResponse)
+def cadastrar_aula():
+    return 
+
+@app.get("/listar_aulas", response_class=HTMLResponse)
+def listar_aulas():
+    return
+
+@app.put("/editar_aula", response_class=HTMLResponse)
+def editar_aula():
+    return 
+
+
+# ----------------------------
+# 3. Filtragem
+# ----------------------------
+
+
+@app.get("/filtrar", response_class=HTMLResponse)
+def filtrar_aula():
+    return
+
+@app.get("/listar_professores", response_class=HTMLResponse)
+def listar_professores():
+    return
+
+@app.get("/filtrar", response_class=HTMLResponse)
+def listar_professores():
+    return
+
+@app.get("/filtrar", response_class=HTMLResponse)
+def ocultar_professores():
+    return
+
+@app.get("/filtrar", response_class=HTMLResponse)
+def filtrar_professor_por_aula():
+    return
+
+
+# ----------------------------
+# 4. Agendamento de Aulas
+# ----------------------------
+@app.get("/listar_hora_aula", response_class=HTMLResponse)
+def listar_hora_aula():
+    return
 
 @app.post("/agendar-aula", response_class=HTMLResponse)
 def agendar_aula():
-    return 
+    return
 
 @app.post("/cancelar-aula", response_class=HTMLResponse)
 def cadastrar_aula():
     return
 
+# ----------------------------
+# 5 . Pagamento
+# ----------------------------
+
+@app.post("/definir_valor", response_class=HTMLResponse)
+def definir_valor():
+    return
+
+@app.delete("/remover_pacote", response_class=HTMLResponse)
+def remover_pacote():
+    return
