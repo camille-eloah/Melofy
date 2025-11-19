1640ab8e1a8fb8831729df8d3f59ab64803cb61a
arianevaleira <costa.valeria@escolar.ifrn.edu.br>
2025-11-19 14:13:37 -0300

    Feat

--- frontend/src/componentes/entrada/Entrada.jsx
+++ frontend/src/componentes/entrada/Entrada.jsx
@@ -1,44 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import "./Entrada.css";
+import teste from "../../assets/teste1.png";
+
 
 function Entrada() {
   const navigate = useNavigate();
 
-  const handleCadastrar = () => {
-    navigate("/cadastro");
-  };
-
-  const handleLogar = () => {
-    navigate("/login");
-  };
-
-
   return (
-    <div className="melofy-container">
-      <div className="melofy-background">
-        <div className="melofy-content">
-          <div className="melofy-logo">
-            <h1>MELOFY</h1>
-          </div>
-          <div className="melofy-slogan">
-            <p>No Melofy, cada nota é um passo na sua</p>
-            <p>jornada musical.</p>
-          </div>
-          <div className="melofy-buttons">
-            <button 
-              className="melofy-btn cadastrar-btn"
-              onClick={handleCadastrar}>
-              Cadastrar
-            </button>
-            <button className="melofy-btn logar-btn"
-              onClick={handleLogar}>
-              Logar
-            </button>
-          </div>
+    <div className="landing-container">
+      {/* Área esquerda - textos */}
+      <div className="landing-left">
+        <h1 className="landing-title">MELOFY</h1>
+
+        <h2 className="landing-subtitle">
+          Soluções inovadoras<br />para sua jornada musical
+        </h2>
+
+        <p className="landing-description">
+          No Melofy, cada nota é um passo na sua evolução musical.
+        </p>
+
+        <div className="landing-buttons">
+          <button className="btn-primary" onClick={() => navigate("/cadastro")}>
+            Cadastrar
+          </button>
+
+          <button className="btn-outline" onClick={() => navigate("/login")}>
+            Logar
+          </button>
+        </div>
+      </div>
+
+      {/* Área da direita – espaço para imagem */}
+      <div className="landing-right">
+        <div className="container-onda"></div>
+        <div className="image-placeholder">
+           <img src={teste} />
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Entrada;
